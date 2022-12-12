e38d25271d134e582e4aa75d8b39136d1a23792d
salvedojuliao <batistajulio215@gmail.com>
2022-12-12 13:25:21 -0300

    echo: add new files

---
+++ roger-melo-cjrm/aula-01/app.js
@@ -0,0 +1,16 @@
+// Com variávels eu consigo alterar o valor delas:
+let age = 31
+let currentYear = 2019
+
+console.log(age, currentYear)
+
+// A partir daqui a variável muda de valor:
+age = 32
+console.log(age)
+
+// Com constante eu não consigo alterar o valor, quando eu quiser que o valor seja constante no decorrer de toda a aplicação:
+const points = 100
+console.log(points)
+
+
+
--- roger-melo-cjrm/notesCJRM.md
+++ roger-melo-cjrm/notesCJRM.md
@@ -1,2 +1,1 @@
-- Resumir os resumos 
-- Usar bastante o github
+
