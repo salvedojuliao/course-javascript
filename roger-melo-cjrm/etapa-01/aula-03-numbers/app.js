c211539053bfc7af5b43fa45dc164da577d4e9ec
salvedojuliao <batistajulio215@gmail.com>
2022-12-13 23:34:50 -0300

    add new files and create a new folder called 'etapa 01' whete I put all the files

---
+++ roger-melo-cjrm/etapa-01/aula-03-numbers/app.js
@@ -0,0 +1,49 @@
+// Inteiros e decimais
+const radius = 10
+const pi = 3.14
+
+// Operadores aritméticos
+const reminder = 5 % 2 // '%' pega o resto da operação
+const area = pi * radius ** 2
+
+// Ordem de operações
+const crazyOperation = 5 * (10 - 3) ** 2
+// 1) Parenteses
+// 2) Expoentes ou raízes
+// 3) Multiplicação e divisão
+// 4) Adição e subtração
+
+// Operadores de incremento e decremento
+let postLikes = 10;
+// postLikes = postLikes + 1; // = postLikes++
+
+// Incremento:
+//postLikes++ // postLikes + 1
+
+// Decremento:
+// postLikes-- // = postLikes - 1
+
+// Operadores addition, subtraction, multiplication e division
+
+// Addition:
+// postLikes = postLikes + 10 // = postLikes += 10
+// postLikes += 10 // = postLikes = postLikes + 10
+
+// Subtraction
+// postLikes -= 5
+
+// Multiplication
+// postLikes *= 3 // = postLikes = postLikes * 3
+
+// Division
+// postLikes /= 2 // = postLikes = postLikes / 2
+
+// NaN - not a number
+// Ele irá aparecer quanto eu tentar fazer algum tipo de operação que não faça sentido. Algum tipo de operação que não resulta em um número
+// ex: // console.log(7 / 'oi');
+
+// Contatenação de string com número
+const likesMessage = 'O post tem' + postLikes + ' likes.'
+
+
+
---
+++ roger-melo-cjrm/etapa-01/aula-04-template-strings/app.js
@@ -0,0 +1,21 @@
+const postTitle = 'É bolacha ou biscoito?'
+const postAuthor = 'Matheus Saad'
+const postComments = 15
+
+// Usando contatenação (Forma S5)
+// const postMessage = 'O post ' + postTitle + '" do' + 'postAuthor' + ', tem '+ postComments + ' comentários';
+// console.log(postMessage)
+
+console.log('Você manja do filme \'As branquelas\'?')
+
+// Usando template strings (Forma S6) 
+// const postMessage = `O post "${postTitle}", do ${postAuthor}, tem ${postComments} comentários`
+// console.log(postMessage)
+
+// Criando templates HTML
+const html = `
+    <h2>${postTitle}</h2>
+    <p>${postAuthor}</p>
+    <span>Este post tem ${postComments} comentários</span>
+`
+console.log(html)
