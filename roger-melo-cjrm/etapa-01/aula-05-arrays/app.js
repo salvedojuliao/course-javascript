50ddf6a31581504aa0cc03efca9e192c116f9780
salvedojuliao <batistajulio215@gmail.com>
2022-12-14 16:53:32 -0300

    add folder of class about Arrays

---
+++ roger-melo-cjrm/etapa-01/aula-05-arrays/app.js
@@ -0,0 +1,15 @@
+// Arrays
+let heroes = ['Batman', 'Catwoman', 'Iron Man'];
+const ages = [31, 25, 39, 40];
+
+// Métodos de arrays
+const joinHeroes = heroes.join('|');
+const indexOf25 = ages.indexOf(25);
+const moreHeros = heroes.concat(['Superman', 'Wolverine']);
+const pushToHoroes = heroes.push('Cyclops', 'Hulk');
+const popHeroes = heroes.pop();
+
+console.log(heroes);
+
+
+
